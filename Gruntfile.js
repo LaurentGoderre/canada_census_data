406c783c62b6736143fdb046eb551e537bbc8dab
Laurent Goderre <laurent.goderre@gmail.com>
2017-11-08 13:22:01 -0500

    Added build

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/* eslint-env node */
+module.exports = function(grunt) {
+	// load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+	require("load-grunt-tasks")(grunt, {pattern: ["grunt-*", "gruntify-*"]});
+
+	grunt.initConfig({
+		eslint: {
+			src: ["src/*.js"]
+		},
+
+		clean: {
+			dist: "dist"
+		},
+
+		browserify: {
+  		dist: {
+				options: {},
+				files: {
+					"dist/canada_census_data.js": "index.js"
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				sourceMap: true
+			},
+			all: {
+				expand: true,
+				cwd: "dist",
+				src: "**/*.js",
+				dest: "dist",
+				ext: ".min.js"
+			}
+		},
+	});
+	grunt.registerTask("default", ["eslint", "clean", "browserify", "uglify"]);
+};
